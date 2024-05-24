70dd83d85cbdfd64fdb6a62f312c8022e930d7a2
Tadiwa Chipolopolo <tadiwanashenicckky@gmail.com>
2024-05-24 10:49:55 +0200

    created the mobile ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import { Box, Container, Grid, Stack, Typography } from "@mui/material";
 import hero from "./components/images/mobile/image-hero.jpg";
 import iterative from "./components/images/mobile/image-interactive.jpg";
+import earth from "./components/images/mobile/image-deep-earth.jpg";
+import night from "./components/images/mobile/image-night-arcade.jpg";
+import soccer from "./components/images/mobile/image-soccer-team.jpg";
+import gridy from "./components/images/mobile/image-grid.jpg";
+import vr from "./components/images/mobile/image-from-above.jpg";
+import pocket from "./components/images/mobile/image-pocket-borealis.jpg";
+import curosity from "./components/images/mobile/image-curiosity.jpg";
+import fisheye from "./components/images/mobile/image-fisheye.jpg";
+import { Button } from "@mui/base";
+
 
 function App() {
   return (
@@ -21,7 +31,7 @@ function App() {
               display: "flex",
               alignItems: "center",
               justifyContent: "center",
-              mb:"3rem"
+              mb: "2rem",
             }}
           >
             <Box>
@@ -31,14 +41,23 @@ function App() {
                 style={{ width: "100%", height: "100%" }}
               />
               <Typography
-                sx={{ textAlign: "center", color: " hsl(0, 0%, 41%)",textTransform:"capitalize" ,mb:"2rem"}}
+                sx={{
+                  textAlign: "center",
+                  color: " hsl(0, 0%, 41%)",
+                  textTransform: "capitalize",
+                  mb: "2rem",
+                }}
               >
                 <h1>The leader in interactive VR</h1>
               </Typography>
               <Typography
-                sx={{ textAlign: "center", color: "hsl(0, 0%, 55%)" }}
+                sx={{
+                  textAlign: "center",
+                  color: "hsl(0, 0%, 55%)",
+                  mb: "1.5rem",
+                }}
               >
-                <p>
+                <p style={{ alignItems: "center" }}>
                   {" "}
                   Founded in 2011, Loopstudios has been producing world-class
                   virtual reality projects for some of the best companies around
@@ -48,6 +67,119 @@ function App() {
                 </p>
               </Typography>
             </Box>
+          </Container>
+          <Container sx={{ mb: "2rem" }}>
+            <Box>
+              <Typography
+                sx={{
+                  textAlign: "center",
+                  color: "hsl(0, 0%, 41%)",
+                  textTransform: "uppercase",
+                }}
+              >
+                <h1>Our creations</h1>
+              </Typography>
+            </Box>
+          </Container>
+          <Container>
+            <Box>
+              <img
+                src={earth}
+                alt="earth"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  marginBottom: "1.5rem",
+                }}
+              />
+            </Box>
+            <img
+              src={night}
+              alt="night"
+              style={{ width: "100%", height: "100%", marginBottom: "1.5rem" }}
+            />
+            <img
+              src={soccer}
+              alt="earth"
+              style={{ width: "100%", height: "100%", marginBottom: "1.5rem" }}
+            />
+            <img
+              src={gridy}
+              alt="night"
+              style={{ width: "100%", height: "100%", marginBottom: "1.5rem" }}
+            />
+            <img
+              src={vr}
+              alt="earth"
+              style={{ width: "100%", height: "100%", marginBottom: "1.5rem" }}
+            />
+            <img
+              src={pocket}
+              alt="night"
+              style={{ width: "100%", height: "100%", marginBottom: "1.5rem" }}
+            />
+            <img
+              src={curosity}
+              alt="earth"
+              style={{ width: "100%", height: "100%", marginBottom: "1.5rem" }}
+            />
+            <img
+              src={fisheye}
+              alt="night"
+              style={{ width: "100%", height: "100%", marginBottom: "1.5rem" }}
+            />
+          </Container>
+          <Box sx={{ display: "flex", justifyContent: "center", mb: "5rem" }}>
+            <Button sx={{ mt: "1rem" }}>
+              <Typography
+                sx={{
+                  p: "1rem",
+                  pl: "2.8rem",
+                  pr: "2.8rem",
+                  height: "0.3rem",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  color: "hsl(0, 0%, 0%)",
+                  textTransform: "uppercase",
+                }}
+              >
+                see all
+              </Typography>
+            </Button>
+          </Box>
+          <Container
+            sx={{
+              backgroundColor: "hsl(0, 0%, 0%)",
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column",
+              alignItems: "center",
+              mb: 3,
+            }}
+          >
+            <Typography sx={{ color: "hsl(0, 0%, 100%)", mt: 4, mb: 3 }}>
+              <h1>Loopstudios</h1>
+            </Typography>
+
+            <Typography sx={{ color: "hsl(0, 0%, 100%)", mb: 3 }}>
+              About
+            </Typography>
+
+            <Typography sx={{ color: "hsl(0, 0%, 100%)", mb: 3 }}>
+              Careers
+            </Typography>
+            <Typography sx={{ color: "hsl(0, 0%, 100%)", mb: 3 }}>
+              {" "}
+              Events
+            </Typography>
+            <Typography sx={{ color: "hsl(0, 0%, 100%)", mb: 3 }}>
+              Products
+            </Typography>
+            <Typography sx={{ color: "hsl(0, 0%, 100%)", mb: 3 }}>
+              Support
+            </Typography>
+            
           </Container>
         </Stack>
       </div>
